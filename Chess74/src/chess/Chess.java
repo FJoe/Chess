6bdbee2aefb6a86d44b2335ea16d8d8020fb0e9f
franc <franc@LAPTOP-LDR2IARU.home>
2017-03-18 15:34:23 -0400

    Small fixes

--- Chess74/src/chess/Chess.java
+++ Chess74/src/chess/Chess.java
@@ -10,17 +10,17 @@ package chess;
 import java.util.Scanner;
 
 public class Chess {
-	
+
 	/**
 	 * Initiates playGame()
 	 * @param args
 	 */
 	public static void main(String[] args){
-		
+
 		playGame();
-		
+
 	}
-	
+
 	/**
 	 * Prints board and takes input from next player
 	 */
@@ -28,94 +28,98 @@ public class Chess {
 	{
 		Board game = new Board();
 		Piece[][] board = game.board;
-		
+
 		Scanner in = new Scanner(System.in);
-		
+
 		//0 = black player's move
 		//1 = white player's move
 		boolean moveType = true;
-		
+
 		//-1 = game continues
 		//0 = black wins
 		//1 = white wins
 		//2 = draw
 		int winner = -1;
-		
+
 		boolean draw = false;
-				
+		boolean newTurn = true;
+
 		while(winner == -1)
 		{
-			game.printBoard();
-			
-			boolean thisTurn = true;
-			while(thisTurn)
+			if(newTurn)
+				game.printBoard();
+			if(moveType)
+				System.out.println("White's move: ");
+			else
+				System.out.println("Black's move: ");
+
+			String input = in.nextLine();
+
+			if(input.equals("resign"))
 			{
 				if(moveType)
-					System.out.println("White's move: ");
+					winner = 0;
 				else
-					System.out.println("Black's move: ");
-				
-				String input = in.nextLine();
-				
-				if(input.equals("resign"))
-				{
-					if(moveType)
-						winner = 0;
-					else
-						winner = 1;
-					
-					thisTurn = false;
-				}
-				
-				else if(input.equals("draw"))
-				{
-					if(draw)
-					{
-						winner = 2;
-						thisTurn = false;
-					}
-					else
-						System.out.println("Illegal move, try again");
-				}
-				
+					winner = 1;					
+			}
+
+			else if(input.equals("draw"))
+			{
+				if(draw)
+					winner = 2;
 				else
 				{
-					int[] moveset = translateFileRank(input);
-					int startX = moveset[0];
-					int startY = moveset[1];
-					int endX = moveset[2];
-					int endY = moveset[3];
-					
-					Piece toMove = board[startY][startX];
-					if(toMove != null)
-					{
-						if((moveType && toMove.color.equals("black")) || 
-								(!moveType && toMove.color.equals("white")))
-							System.out.println("Illegal move, try again");
-						else
-						{
-							if(toMove.tryMove(endX, endY, board))
-							{
-								toMove.move(endX, endY, board);
-								thisTurn = false;
-								
-								if(moveset[4] == 1)
-									draw = true;
-								else
-									draw = false;
-							}
-							else
-								System.out.println("Illegal move, try again");
-						}
-					}
-					else
-						System.out.println("Illegal move, try again");
+					System.out.println("Illegal move, try again");
+					newTurn = false;
 				}
 			}
-			moveType = !moveType;
-			
-		}//end while loop
-		
+
+			else
+			{
+				int[] moveset = translateFileRank(input);
+				int startX = moveset[0];
+				int startY = moveset[1];
+				int endX = moveset[2];
+				int endY = moveset[3];
+
+				Piece toMove = board[startY][startX];
+				if(toMove != null)
+				{
+					if((moveType && toMove.color.equals("black")) || 
+							(!moveType && toMove.color.equals("white")))
+					{
+						System.out.println("Illegal move, try again");
+						newTurn = false;
+					}
+					else
+					{
+						if(toMove.tryMove(endX, endY, board))
+						{
+							toMove.move(endX, endY, board);
+							newTurn = true;
+							moveType = !moveType;
+
+							if(moveset[4] == 1)
+								draw = true;
+							else
+								draw = false;
+						}
+						else
+						{
+							System.out.println("Illegal move, try again");
+							newTurn = false;
+						}					}
+				}
+				else
+				{
+					System.out.println("Illegal move, try again");
+					newTurn = false;
+				}
+			}
+		}
+
+		//end while loop
+
 		if(winner == 0)
 			System.out.println("Black wins!");
 		else if(winner == 1)
@@ -123,7 +127,7 @@ public class Chess {
 		else if(winner == 2)
 			System.out.println("Draw");
 	}//end playGame()
-	
+
 	/**
 	 * translateFileRank
 	 * Converts file/rank array numbering into the 
@@ -142,10 +146,7 @@ public class Chess {
 
 		if(oldFR.endsWith("draw?"))
 			newFR[4] = 1;
-		
-		for(int i = 0; i < 5; i++)
-			System.out.println(newFR[i]);
-		
+
 		return newFR;
 	}
 }
